96d1efb5cb215f4d24e35955d7dda7202192f039
Dayana <dayana82006@gmail.com>
2024-10-04 09:56:50 -0500

    py

--- main.py
+++ main.py
@@ -3,19 +3,19 @@ import sys
 import modules as modulo
 
 if __name__=='__main__':
-    print(modulo.menu)
+    
     isActive=True
     while isActive:
-        try:
-            opcion=input('Escriba aqui la opcion')
+        #try:
+            print(modulo.menu)
+            opcion=int(input('Escriba aqui la opcion: '))
             match opcion:
                 case 1:
-                    modulo.registrarJ()
                     modulo.jugarPc()
+                    
                 
                 
                 
                 
-                
-        except:
-            input('Elija una opcion correcta ')
+        #except:
+            #input('Elija una opcion correcta ')
--- modules/pc.py
+++ modules/pc.py
@@ -1,20 +1,54 @@
 import os 
 import sys
 import modules as mdls
+import random
+
 contador={
     'PuntosPc':0,
     'PuntosUs':0,
-    'rondas':0,
+    'rondasGU':0,
+    'rondasGM':0,
     'resultados':0
 }
-def registrarJ():
-    user=(input('🃟 Registre su nombre de usuario'))
-    users={
-        'name':user
-    }
 
 def jugarPc():
-    opcUs=str(input(''))
-    opcPc=['Piedra', 'Papel', 'Tijera']
-    
-    
+    print('USTED JUGARA CONTRA EL PC')
+    isActive=True
+    user=str(input('🃟 Registre su nombre de usuario: '))
+    users={
+            'name':user
+            }
+    while isActive:
+        print(mdls.opcPC)
+        opcUs=(input('ELije sabiamente: ')).lower()
+        maquina=['piedra', 'papel', 'tijera']
+        opcionPc=random.choice(maquina)
+        if contador['rondasGU']<3 or contador['rondasGM']<3:
+            if opcUs=='piedra' and opcionPc=='tijera':
+                print('Has ganado')
+                contador['rondasGU']+=1
+            elif opcUs=='papel' and opcionPc=='piedra':
+                print('Has ganado')
+                contador['rondasGU']+=1
+            elif opcUs=='tijera' and opcionPc=='papel':
+                    print('Has ganado')
+                    contador['rondasGU']+=1
+            if opcionPc=='piedra' and opcUs=='tijera':
+                print('Has perdido')
+                contador['rondasGM']+=1
+            elif opcionPc=='papel' and opcUs=='piedra':
+                print('Has perdido')
+                contador['rondasGM']+=1
+            elif opcionPc=='tijera' and opcUs=='papel':
+                print('Has perdido')
+                contador['rondasGM']+=1
+            elif opcUs==opcionPc:
+                print('Empate')
+            elif opcUs != maquina:
+                print('digite un dato correcto')
+                input('Presione cualquier tecla para volver a intentar...')
+            else:
+                print('limite')
+                isActive=False
+        else:
+            isActive=False
